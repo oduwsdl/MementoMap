44f98c7319b1c01c4aede369b0751515d0dff80c
Sawood Alam <ibnesayeed@gmail.com>
2019-01-19 22:28:08 -0500

    Add basic code structure

---
+++ main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from mementomap.mementomap import MementoMap
+
+
+if __name__ == "__main__":
+    mm = MementoMap()
---
+++ mementomap/mementomap.py
@@ -0,0 +1,29 @@
+class MementoMap():
+    """MementoMap is a class that provides methods to interact with and maintain MementoMap files"""
+
+    def __init__(self):
+        """Initialize a MementoMap instance"""
+        self.depthstats = {
+            "hosts": [
+                (1.00000,  973.00),
+                (0.99818, 2287.98),
+                (0.55153,    8.53),
+                (0.17559,    8.95),
+                (0.03438,    7.77),
+                (0.01534,    6.28),
+                (0.00843,    3.42),
+                (0.00554,    4.55),
+                (0.00229,    1.00)
+            ],
+            "paths": [
+                (0.86817,   25.00),
+                (0.64156,    7.25),
+                (0.35412,    4.96),
+                (0.21621,    3.26),
+                (0.11310,    3.29),
+                (0.05993,    2.70),
+                (0.02714,    2.48),
+                (0.01237,    2.09),
+                (0.00387,    2.01)
+            ]
+        }
